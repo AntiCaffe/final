c2d17d8977dc88e38a132b11588f4315c093384b
AntiCaffe <dongjun6288@gmail.com>
2022-05-23 02:47:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.afinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val fragmentOne by lazy { dateFragment() }
+    private val fragmentTwo by lazy { mainFragment() }
+    private val fragmentThree by lazy {memoFragment()}
+
+
+
+
+    private val items = mutableListOf<ContentsModel>()  //데이터 모델 넣을 리스트.
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initNavigationBottom()
+        changeFragment(fragmentOne)
+
+        setSupportActionBar(main_layout_toolbar) // 툴바를 액티비티의 앱바로 지정
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 드로어를 꺼낼 홈 버튼 활성화
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24) // 홈버튼 이미지 변경
+        supportActionBar?.setDisplayShowTitleEnabled(true) // 툴바에 타이틀 보이게
+
+
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_message -> Toast.makeText(applicationContext, "Clicked Message", Toast.LENGTH_SHORT).show()
+                R.id.nav_sync -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_trash -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_setting -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_login -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_rate_us -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->{ // 메뉴 버튼
+                drawerLayout.openDrawer(GravityCompat.START)    // 네비게이션 드로어 열기
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initNavigationBottom() {
+        bnv_main.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.first -> {
+                        changeFragment(fragmentOne)
+                    }
+                    R.id.second -> {
+                        changeFragment(fragmentTwo)
+                    }
+                    R.id.third -> {
+                        changeFragment(fragmentThree)
+                    }
+                }
+                true
+            }
+
+
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_container, fragment).commit()
+    }
+
+    override fun onBackPressed() { //뒤로가기 처리
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawers()
+            // 테스트를 위해 뒤로가기 버튼시 Toast 메시지
+            Toast.makeText(this,"back btn clicked",Toast.LENGTH_SHORT).show()
+        } else{
+            super.onBackPressed()
+        }
+    }
+}
