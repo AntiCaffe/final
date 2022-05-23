a0bb0292b7956619aae8f56a2ac3384a60decc9a
AntiCaffe <dongjun6288@gmail.com>
2022-05-23 14:07:12 +0900

    final ver3

--- app/src/main/java/com/example/afinal/MainActivity.kt
+++ app/src/main/java/com/example/afinal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.afinal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
@@ -28,6 +29,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         initNavigationBottom()
         changeFragment(fragmentOne)
